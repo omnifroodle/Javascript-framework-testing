058181f26ce635412ee73f0618d8d8ca07cba2dd
Christopher Bradford <bradfordcp@gmail.com>
2011-01-12 10:12:10 -0500

    Basic tab bar and cards set up

---
+++ static/javascripts/sencha/senchaapp.js
@@ -0,0 +1,38 @@
+Ext.setup({
+  tabletStartupScreen: '',
+  phoneStartupScreen: '',
+  icon: '',
+  glossOnIcon: false,
+  onReady: function () {
+    var converter_card = new Ext.Component({
+      title: 'Converter',
+      cls: 'converter',
+      scroll: 'vertical',
+      tpl: [
+      ]
+    });
+    
+    var rates_card = new Ext.Component({
+      title: 'Rates',
+      cls: 'rates',
+      scroll: 'vertical',
+      tpl: [
+        '<tpl for=".">',
+          '<div class="rate">',
+            '<div class="name">{name}</div>',
+            '<div class="symbol">{symbol}</div>',
+            '<div class="rate">{rate}</div>',
+          '</div>',
+        '</tpl>'
+      ]
+    });
+    
+    var panel = new Ext.TabPanel({
+      fullscreen: true,
+      cardAnimation: 'slide',
+      items: [converter_card, rates_card]
+    });
+    
+    
+  }
+});
